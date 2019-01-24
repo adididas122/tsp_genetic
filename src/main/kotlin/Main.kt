4739a4a6972c4200a23586212932c8947d7b313b
Adrian <adrian.kunowski@gmail.com>
2019-01-24 21:07:19 +0100

    Add arguments when start jar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.Options
 import graphic.DrawLines
 import kotlin.system.measureTimeMillis
 
@@ -9,6 +11,7 @@ object Main {
     @JvmStatic
     fun main(args: Array<String>) {
 
+        initProperties(args)
         val algorithm = Algorithm(geneNumber, populationSize)
         val time = measureTimeMillis {
             repeat(maxGeneration) {
@@ -21,5 +24,19 @@ object Main {
         println("Shortest path: ${algorithm.population.selectParent().first.score}")
         DrawLines.draw(algorithm.population.selectParent().first.chromosome)
 
+    }
+
+    private fun initProperties(args: Array<String>) {
+        val options = Options()
+        options.addOption("g", true, "number of genes")
+        options.addOption("p", true, "population size")
+        options.addOption("m", true, "number of generations")
+
+        val parser = DefaultParser()
+        val cmd = parser.parse(options, args)
+
+        if (cmd.getOptionValue("g") != null) geneNumber = cmd.getOptionValue("g").toInt()
+        if (cmd.getOptionValue("p") != null) populationSize = cmd.getOptionValue("p").toInt()
+        if (cmd.getOptionValue("m") != null) maxGeneration = cmd.getOptionValue("m").toInt()
     }
 }
